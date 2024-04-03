f84c4a333e09e04a14a7c40181e8d806809f2c78
Maulana Tegar I <tegarmaulana973@gmail.com>
2024-04-04 00:45:35 +0700

    JWT SECRET pagination

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,11 +36,16 @@ const register = async (req, res, next) => {
 
     console.log(req.user.shopId);
 
+    let shopId = req.user.shopId
+    if(req.user.role === "Admin" && req.body.shopId){
+      shopId = req.user.shopId
+    }
+
     const newUser = await User.create({
       name,
       address,
       age,
-      shopId: req.user.shopId,
+      shopId,
     });
     const test = await Auth.create({
       email,
@@ -84,6 +89,7 @@ const login = async (req, res, next) => {
           username: user.User.name,
           role: user.User.role,
           email: user.email,
+          shopId: user.shopId,
         },
         process.env.JWT_SECRET,
         {
